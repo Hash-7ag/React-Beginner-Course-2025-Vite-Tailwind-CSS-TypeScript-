0167a1ba08775951c31bd91c7b5b76cf58cd7fce
Nan0-S <nan.fgbs@gmail.com>
2025-12-14 15:44:36 +0400

    better ui, add TodoItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
+import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
 
 function App() {
 
   return (
     <main className='py-10 h-screen'>
-      <h1 className='font-bold text-3xl text-center'>Your Todos</h1>
-      <div className="max-w-lg mx-auto">
+      <h1 className='font-bold text-3xl text-center text-slate-900 pb-4'>Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 p-10 rounded-xl">
          <div className="space-y-2">
             {dummyData.map(todo=> (
-               <p key={todo.id} className=" text-xl text-gray-800">
-                  {todo.title}
-               </p>
+               <TodoItem todo={todo} />
             ))}
          </div>
       </div>
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,25 @@
+import type { Todo } from "../types/todo"
+
+interface TodoItemProps {
+   todo: Todo;
+}
+
+function TodoItem({todo}: TodoItemProps) {
+   return (
+      <div key={todo.id}>
+         <label className="
+         flex items-center gap-2 
+         border rounded-xl border-slate-700 
+         py-1 px-3 
+         bg-white hover:bg-slate-200 hover:scale-[1.032] transition-all ">
+            <input 
+            type="checkbox" 
+            className="scale-125 hover:scale-[1.35] transition-all accent-slate-700"  
+            />  
+            <span className={todo.completed ? "line-through text-xl text-slate-400" : "text-xl text-slate-700"}>{todo.title}</span> 
+         </label>
+      </div>
+   )
+}
+
+export default TodoItem

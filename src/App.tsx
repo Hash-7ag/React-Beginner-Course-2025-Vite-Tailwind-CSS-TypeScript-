435e4c1e6c36ccc579187b732f18253f8d4897b8
Nan0-S <nan.fgbs@gmail.com>
2025-12-15 18:31:23 +0400

    useState + updating state in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
 
 function App() {
+   const [todos, setTodos] = useState(dummyData);
 
    function setTodoCompleted(id: number, completed: boolean) {
-      alert(`Todo with id: ${id} is now ${completed ? "completed" : "cot completed"}`)
+      // alert(`Todo with id: ${id} is now ${completed ? "completed" : "cot completed"}`);
+      setTodos((prevTodos) => 
+         prevTodos.map(todo => (
+            todo.id === id ? {...todo, completed} : todo
+         ))
+      );
    } 
 
   return (
@@ -12,7 +19,7 @@ function App() {
       <h1 className='font-bold text-3xl text-center text-slate-900 pb-4'>Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 p-10 rounded-xl">
          <div className="space-y-2">
-            {dummyData.map(todo=> (
+            {todos.map(todo=> (
                <TodoItem key={todo.id} todo={todo} onCompletedChange={setTodoCompleted}/>
             ))}
          </div>

d10a51518664a530fb7019bc2fdf7835bfd4d7e9
Nan0-S <nan.fgbs@gmail.com>
2025-12-27 22:35:29 +0400

    fix: truncate long todo titles with ellipsis

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -11,17 +11,23 @@ function TodoItem({todo, onCompletedChange, onDelete}: TodoItemProps) {
    return (
       <div className="flex items-center gap-1 ">
          <label className="
-         flex items-center gap-2 grow 
-         border-2 rounded-xl border-slate-500 
-         py-1 px-3 
-         bg-white hover:bg-slate-100 hover:scale-[1.032] transition-all ">
+         flex items-center gap-2 grow min-w-0
+         border-2 rounded-xl border-slate-500
+         py-1 px-3
+         bg-white hover:bg-slate-100 hover:scale-[1.032] transition-all">
             <input 
             type="checkbox" 
             checked={todo.completed}
             onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
             className="scale-125 hover:scale-[1.35] transition-all accent-slate-500"  
             />  
-            <span className={todo.completed ? "line-through text-mid sm:text-xl text-slate-400" : "text-mid sm:text-xl text-slate-500"}>{todo.title}</span> 
+            <span
+            className={`line-clamp-3 break-words text-mid sm:text-xl ${
+                  todo.completed
+                    ? "line-through text-slate-400"
+                    : "text-slate-500"
+               }`}
+            >{todo.title}</span> 
          </label>
          <button 
             className="p-0 md:p-2"
